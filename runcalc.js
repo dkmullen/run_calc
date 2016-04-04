179393a1de0cef5a1c9d5b2a144d53d2164ac655
dkmullen <dkmullen@gmail.com>
2016-04-04 17:57:31 -0400

    docs: Finish jsdocs comments, also remove one redundant line for zeroing out seconds

--- runcalc.js
+++ runcalc.js
@@ -13,37 +13,33 @@ function ViewModel() {
 	 * button in the DOM. Checks that data has been entered for two of the 
 	 * three categories (miles, elapsed time, pace). Specifically checks for
 	 * miles, elapsed minutes and pace minutes, but if user enters elapsed 
-	 * hours, minutes and seconds are filled in with zero. Likewise if user
-	 * enters only pace minutes. Entering only seconds or pace seconds throws
-	 * the error message. Finally, calls the function for the missing field.
+	 * hours (or minutes), fills in minutes (or hours) with zero. 
+	 * Likewise if user enters only pace minutes. Entering only seconds or
+	 * pace seconds throws the error message. Finally, calls the function
+	 * for the missing field.
 	 @function
 	 */
 	this.calcMyRun = function() {
 		if ((hours() !== undefined && minutes() == undefined)) {
 			minutes('00');
 		}
-		if ((hours() !== undefined && seconds() == undefined)) {
-			seconds('00');
-		}
 		if (hours() == undefined && minutes() !== undefined) {
 			hours('0');
 		}
+		
 		if ((miles() == undefined && minutes() == undefined) || 
 			(miles() == undefined && paceMinutes() == undefined) ||
 			(minutes() == undefined && paceMinutes() == undefined)) {
 			/** Uses a ko.observable in HTML to change color if true */
 			errorMessage(true);
-		}
-		else if (miles() == undefined) {
+		} else if (miles() == undefined) {
 			/** In case err message was red from previous try, this resets it */
 			errorMessage(false);
 			this.milesCalc();
-		}
-		else if (minutes() == undefined) {
+		} else if (minutes() == undefined) {
 			errorMessage(false);
 			this.timeCalc();
-		}
-		else {
+		} else {
 			errorMessage(false);
 			this.paceCalc();
 		}
@@ -54,6 +50,7 @@ function ViewModel() {
 	 * If number < 10, adds a '0' to the front, otherwise adds empty string
 	 * @function
 	 * @param {number} num - Passed in from other functions
+	 * @returns a two-digit number 
 	 */
 	this.pad2 = function(num) {
 		return (num < 10 ? '0' : '') + num;
@@ -65,13 +62,19 @@ function ViewModel() {
 	 * back two places by multiplying by 10 to the minus 2
 	 * @function
 	 * @param {number} num - Passed in from other functions
+	 * @returns a number rounded to two decimal places
 	 */
 	this.roundToTwo = function(num) {
 		return +(Math.round(num + 'e+2')  + 'e-2');
 	}
 	
 	/**
-	 * Function to calculate the miles field, called by calcMyRun. 
+	 * Function to calculate the miles field, called by calcMyRun. Fills in
+	 * seconds and/or pace seconds with '00' if needed, parses the numbers from
+	 * str type to int, coverts them all to seconds and adds them, does the same
+	 * with pace, divides totalSeconds by secondsPerMile, rounds to two decimal
+	 * places by calling roundToTwo function.
+	 * @function
 	 */
 	this.milesCalc = function() {
 		if (seconds() == undefined) {
@@ -88,6 +91,15 @@ function ViewModel() {
 		miles(num);
 	}
 	
+	/** 
+	 * Function to calculate elapsed time, called by calcMyRun. Fills in 
+	 * paceSeconds with '00' if needed, parses numbers in string type from form
+	 * to number type, calculates total seconds for run, divides by 3600 
+	 * (seconds in an hour), uses the 'floor' for hours, calcs minutes from
+	 * floor of remaining seconds div by 60, calcs seconds from the remainder,
+	 * rounding to the nearest second, calls pad2 so result is two digits.
+	 * @function
+	 */
 	this.timeCalc = function() {
 		if (paceSeconds() == undefined) {
 			paceSeconds('00');
@@ -101,7 +113,18 @@ function ViewModel() {
 		seconds(this.pad2(Math.round(remainingSeconds % 60)));
 		
 	}
-	
+
+	/** 
+	 * Function to calculate pace, called by calcMyRun. Fills in seconds with
+	 * '00' if needed, parses numbers in string type from form to number type,
+	 * calculates total seconds for run and seconds per mile, calcs pace minutes
+	 * by finding floor of secPerMile/60, calcs paceSeconds by secPerMile/60,
+	 * subtracts the mines, multiplies the resulting decimal by 60 to convert
+	 * from decimal to seconds, rounds and pads it.
+	 * 'If' statement covers the case where paceSeconds rounds up to 60. Adds
+	 * 1 to paceMinutes, sets paceSeconds to 0.
+	 * @function
+	 */	
 	this.paceCalc = function() {
 		if (seconds() == undefined) {
 			seconds('00');
@@ -119,8 +142,11 @@ function ViewModel() {
 		}
 	}
 
-
-	
+	/** 
+	 * Function to reset form in DOM, set all fields to undefined so no data
+	 * remains. Called by knockout.js data-bind on 'Reset' button.
+	 * @function
+	 */
 	this.formReset = function() {
 		miles(undefined);
 		hours(undefined);
@@ -134,17 +160,17 @@ function ViewModel() {
 }
 ko.applyBindings(new ViewModel());
 
-/** pad2 comes from fanaur at
-
-http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit
-
-It means: 
-if (number < 10) {
-	return '0' + number;
-	}
-	else {
-		return '' + number;
-	}
-	
-http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript
-*/
+/** 
+ * Credits: 
+ * pad2 function comes from fanaur at:
+ * http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit
+ *
+ * It means: 
+ * if (number < 10) {return '0' + number;}
+ *	else {return '' + number;}
+ *
+ * roundToTwo function comes from MarkG at:
+ * http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript
+ */
+ 
+ 
